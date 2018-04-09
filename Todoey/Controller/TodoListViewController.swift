f9678244233411696fb881b65bedc03a8c0ac2ef
Aleksandrov Aleksandr <light.it.zp@gmail.com>
2018-04-09 08:31:47 +0300

    Search bar with searching

--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -96,14 +96,25 @@ class TodoListViewController: UITableViewController {
         tableView.reloadData()
     }
     
-    func loadItems() {
-        let request : NSFetchRequest<Task> = Task.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Task> = Task.fetchRequest()) {
+        
         do {
             itemArray = try context.fetch(request)
         } catch {
             print("Error fetching data from context \(error)")
         }
+    }
+}
+
+//MARK: - Search bar methods
+extension TodoListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Task> = Task.fetchRequest()
         
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
     }
 }
